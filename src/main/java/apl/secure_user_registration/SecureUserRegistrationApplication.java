117af765df4d0d27f3a0c198accf0c77158f8da5
Pedro Oliveira <pedro_lso@live.com>
2025-10-28 11:13:19 +0000

    Initial commit

---
+++ src/main/java/apl/secure_user_registration/SecureUserRegistrationApplication.java
@@ -0,0 +1,13 @@
+package apl.secure_user_registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureUserRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureUserRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/apl/secure_user_registration/SecureUserRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package apl.secure_user_registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureUserRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
